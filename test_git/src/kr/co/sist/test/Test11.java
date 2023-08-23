e8af06b0fc1be75cb5ea837dfabb458284786fa0
User <User@DESKTOP-2L8891O>
2023-08-23 22:55:35 +0900

    옙

---
+++ test_git/src/kr/co/sist/test/Test11.java
@@ -0,0 +1,10 @@
+package kr.co.sist.test;
+
+public class Test11 {
+
+	public static void main(String[] args) {
+		System.out.println("ㅎㅎㅎㅎㅎㅎㅎ");
+		System.out.println("ㅎㅎㅎㅎㅎㅎㅎ22222222222");
+	}
+
+}
