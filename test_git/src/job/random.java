4cbe3915143c2219c52bdfd4d8493ce894eea405
sssssssssong218 <95688049+sssssssssong218@users.noreply.github.com>
2023-09-09 23:43:06 +0900

    ㅎㅎㅎㅎ

--- test_git/src/job/random.java
+++ test_git/src/job/random.java
@@ -1,10 +1,11 @@
 package job;
 import java.util.Arrays;
+
 import java.util.Random;
 
-public class UseRandom2 {
+public class random {
 	
-	public UseRandom2() {
+	public random() {
 		
 	}
 	//1. 1~45사이의 수를 6개 발생시켜 배열에 넣고 반환하는 일//일을 하는 method
@@ -13,11 +14,10 @@ public class UseRandom2 {
 		int[] lotto=new int[6];
 		Random ran=new Random();
 		
-		int num=0;
 		for(int i =0;i<lotto.length;i++) {
 			lotto[i]=ran.nextInt(45)+1;//발생한 난수를 마지막방에 넣고
 			for(int j =0;j<i;j++) {//발생된 난수가 이전 방에 들어있는지 확인
-				if(lotto[j]==num) {//같은 값이 있다면
+				if(lotto[j]==lotto[i]) {//같은 값이 있다면
 					i--;//바깥 for에서 사용하는 index를 감소시키고
 					break;//안쪽 for를 빠져 나간다.
 				}//end if
@@ -37,7 +37,7 @@ public class UseRandom2 {
 	
 	public static void main(String[] args) {
 		//3. 2번 method를 호출하여 로또번호를 출력한다.
-		UseRandom2 ur2=new UseRandom2();
+		random ur2=new random();
 		ur2.printLotto();
 	}//main
 }//class
