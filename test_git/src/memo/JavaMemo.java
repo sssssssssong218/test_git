c62b41903a548d50368b147a68e81dd6147a335b
user <user@DESKTOP-COJ9858>
2023-09-15 00:52:03 +0900

    메모장

--- test_git/src/memo/JavaMemo.java
+++ test_git/src/memo/JavaMemo.java
@@ -27,7 +27,7 @@ public class JavaMemo extends JFrame {
 		
 		//메뉴
 		JMenu jmFile = new JMenu("파일");
-		
+		//메모장 생각
 		jmiNew = new JMenuItem("새글");
 		jmiOpen = new JMenuItem("열기");
 		jmiSave = new JMenuItem("저장");
