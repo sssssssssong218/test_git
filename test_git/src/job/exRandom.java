679350faf48b71f57f660dd3d2213ea10d90dd17
sssssssssong218 <95688049+sssssssssong218@users.noreply.github.com>
2023-09-25 22:38:03 +0900

    ㅎㅎㅎ

--- test_git/src/job/exRandom.java
+++ test_git/src/job/exRandom.java
@@ -13,7 +13,7 @@ public class exRandom {
 		int[] lotto=new int[6]; 
 		Random random=new Random();
 		  
-		for(int i =0;i<lotto.length;i++) { 
+		for(int i =0;i<lotto.length;i++) {  
 			for(int j=0;j<i;j++) {
 				if(lotto[j]==lotto[i]) {
 					i--;
