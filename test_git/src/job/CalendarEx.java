52041119cfd4bab17589fafa2a0dfef53981c973
sssssssssong218 <95688049+sssssssssong218@users.noreply.github.com>
2023-10-10 00:09:23 +0900

    ggg

--- test_git/src/job/CalendarEx.java
+++ test_git/src/job/CalendarEx.java
@@ -7,7 +7,7 @@ public class CalendarEx {
 	public static void createCalendar(int year,int month) {
 		Calendar cal=Calendar.getInstance();
 		cal.set(year, month-1, 1);//month는 0 ~ 11월 이렇게 12달이 나오기 때문에 8월을 원하면 7이 입력되야한다.
-		
+		   
 		int maxDay=cal.getActualMaximum(Calendar.DAY_OF_MONTH);//그 달의 마지막 일을 찾아준다.
 		int startDay=cal.get(Calendar.DAY_OF_WEEK);//시작요일 찾아줌
 		System.out.println("  일   월   화   수   목  금   토");   
