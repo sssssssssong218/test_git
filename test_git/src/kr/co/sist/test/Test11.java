b03d4ba6226189d62830b3c0b201813e8652a79a
sssssssssong218 <95688049+sssssssssong218@users.noreply.github.com>
2023-10-09 23:58:54 +0900

    ㅎㅎㅎ

--- test_git/src/kr/co/sist/test/Test11.java
+++ test_git/src/kr/co/sist/test/Test11.java
@@ -5,6 +5,7 @@ public class Test11 {
 	public static void main(String[] args) {
 		System.out.println("ㅎㅎㅎㅎㅎㅎㅎ");
 		System.out.println("ㅎㅎㅎㅎㅎㅎㅎ22222222222");
+		System.out.println("테스트중");
 	}
 
 } 
